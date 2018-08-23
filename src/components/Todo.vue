1dc17fefd1038f7332e0930deb8f4b68788a2b30
eevabec <evan.bechtol@ericsson.com>
2018-08-23 13:52:09 -0500

    Fix Due Date Issue
    - Resolve issue where due date was 1 day behind

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -199,6 +199,7 @@
       addTodo () {
         function formatDueDate ( date ) {
           let d = new Date( date );
+          d.setDate( d.getDate() + 1 );
           return d.toDateString();
         }
 
